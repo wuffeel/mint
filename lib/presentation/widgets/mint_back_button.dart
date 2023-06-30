b02cf304969c891d3eaf058ca32a35a72d863f0f
wuffeel <vbl06402@gmail.com>
2023-06-30 12:56:27 +0300

    Remove unused imports

--- lib/presentation/widgets/mint_back_button.dart
+++ lib/presentation/widgets/mint_back_button.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mint/gen/assets.gen.dart';
 import 'package:mint/theme/mint_text_styles.dart';
 
--- lib/theme/mint_theme_data.dart
+++ lib/theme/mint_theme_data.dart
@@ -1,9 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mint/gen/colors.gen.dart';
-import 'package:mint/gen/fonts.gen.dart';
 
 class MintThemeData {
   static const appBarThemeLight = AppBarTheme(
