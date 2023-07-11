25038f63b227ec649ef3c0ea291adc4863b9193a
wuffeel <vbl06402@gmail.com>
2023-07-11 18:54:21 +0300

    Add non-nullable date converter

--- lib/utils/date_utils.dart
+++ lib/utils/date_utils.dart
@@ -1,6 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DateUtils {
+  static DateTime convertToDateTime(dynamic value) {
+    if (value is DateTime) {
+      return value;
+    } else if (value is Timestamp) {
+      return value.toDate();
+    } else if (value is String) {
+      return DateTime.parse(value);
+    }
+    throw ArgumentError.value(value);
+  }
+
   static DateTime? tryConvertToDateTime(dynamic value) {
     if (value is DateTime) {
       return value;
