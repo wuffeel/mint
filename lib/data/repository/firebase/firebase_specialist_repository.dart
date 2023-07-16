6f6b53d19814b9a617c3ab38c4748897d08990c0
wuffeel <vbl06402@gmail.com>
2023-07-16 20:18:06 +0300

    Update reviews order by date to descending

--- lib/data/repository/firebase/firebase_specialist_repository.dart
+++ lib/data/repository/firebase/firebase_specialist_repository.dart
@@ -107,7 +107,7 @@ class FirebaseSpecialistRepository implements SpecialistRepository {
   Future<List<ReviewModelDto>> getSpecialistReviews(String specialistId) async {
     final reviewsSnapshot = await _reviewCollectionRef
         .where('specialistId', isEqualTo: specialistId)
-        .orderBy(_orderByDate)
+        .orderBy(_orderByDate, descending: true)
         .get();
 
     return reviewsSnapshot.docs
