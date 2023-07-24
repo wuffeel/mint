559d4826a089cb28e7e80fa6498fc8a6de5afec7
wuffeel <vbl06402@gmail.com>
2023-07-24 18:28:31 +0300

    Rename factory

--- lib/assembly/model/booking_data_to_dto.dart
+++ lib/assembly/model/booking_data_to_dto.dart
@@ -5,7 +5,7 @@ import '../../data/model/booking_data_dto/booking_data_dto.dart';
 import '../../domain/entity/booking_data/booking_data.dart';
 
 @Injectable(as: Factory<BookingDataDto, BookingData>)
-class BookingModelToDto implements Factory<BookingDataDto, BookingData> {
+class BookingDataToDto implements Factory<BookingDataDto, BookingData> {
   @override
   BookingDataDto create(BookingData param) => BookingDataDto(
         specialistId: param.specialistId,
