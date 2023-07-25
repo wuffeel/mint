46d5886f63ac416a11a7fafd71477f324a1e3d9a
wuffeel <vbl06402@gmail.com>
2023-07-25 13:36:47 +0300

    Remove unused import

--- lib/presentation/pages/main/specialists/widgets/filter_app_bar.dart
+++ lib/presentation/pages/main/specialists/widgets/filter_app_bar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mint/l10n/l10n.dart';
 import 'package:mint/presentation/widgets/bottom_sheet_app_bar.dart';
 
