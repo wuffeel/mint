755bb381221500d2028c6ec76f7e388a03ba7f38
wuffeel <vbl06402@gmail.com>
2023-07-26 18:15:32 +0300

    Add animation to pick-up flow

--- lib/presentation/pages/main/pick_up_specialist/widgets/pick_up_stepper.dart
+++ lib/presentation/pages/main/pick_up_specialist/widgets/pick_up_stepper.dart
@@ -28,6 +28,46 @@ class PickUpStepper extends StatefulWidget {
 }
 
 class _PickUpStepperState extends State<PickUpStepper> {
+  final _pageController = PageController();
+
+  /// Determine whether to call onNextStep or onFinish
+  VoidCallback? get _nextAction =>
+      widget.currentStep + 1 < widget.steps.length
+          ? widget.onNextStep != null
+              ? _onNextStep
+              : null
+          : widget.onFinish;
+
+  void _onNextStep() {
+    final onNext = widget.onNextStep;
+    if (widget.currentStep + 1 < widget.steps.length) {
+      if (onNext != null) onNext();
+      _pageController.animateToPage(
+        widget.currentStep + 1,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+
+  void _onPreviousStep() {
+    final onPrevious = widget.onPreviousStep;
+    if (widget.currentStep > 0) {
+      if (onPrevious != null) onPrevious();
+      _pageController.animateToPage(
+        widget.currentStep - 1,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -39,7 +79,7 @@ class _PickUpStepperState extends State<PickUpStepper> {
           children: List.generate(
             widget.steps.length,
             (index) => Expanded(
-              child: Container(
+              child: AnimatedContainer(
                 height: 3.h,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10.r),
@@ -47,6 +87,8 @@ class _PickUpStepperState extends State<PickUpStepper> {
                       ? Theme.of(context).colorScheme.primary
                       : Theme.of(context).dividerColor,
                 ),
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
               ),
             ),
           )..insertBetween(SizedBox(width: 8.w)),
@@ -54,7 +96,7 @@ class _PickUpStepperState extends State<PickUpStepper> {
         SizedBox(height: 24.h),
         if (widget.currentStep != 0) ...[
           InkWell(
-            onTap: widget.onPreviousStep,
+            onTap: _onPreviousStep,
             child: Row(
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
@@ -65,11 +107,16 @@ class _PickUpStepperState extends State<PickUpStepper> {
           ),
           SizedBox(height: 16.h),
         ],
-        Expanded(child: widget.steps[widget.currentStep]),
+        Expanded(
+          child: PageView(
+            clipBehavior: Clip.none,
+            controller: _pageController,
+            physics: const NeverScrollableScrollPhysics(),
+            children: widget.steps,
+          ),
+        ),
         ElevatedButton(
-          onPressed: widget.currentStep + 1 < widget.steps.length
-              ? widget.onNextStep
-              : widget.onFinish,
+          onPressed: _nextAction,
           child: Text(context.l10n.next),
         ),
       ],
