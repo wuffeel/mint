2c7f8aefea82bc348482bc67aef588dd4c713d4a
wuffeel <vbl06402@gmail.com>
2023-08-08 10:34:26 +0300

    Move stripe config data to .env file

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_firebase_chat_core/flutter_firebase_chat_core.dart';
 import 'package:flutter_libphonenumber/flutter_libphonenumber.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
@@ -9,12 +10,11 @@ import 'package:mint/presentation/app/app.dart';
 import 'injector/injector.dart';
 
 Future<void> main() async {
-  // TODO(wuffel): where this one should be moved?
-  Stripe.publishableKey = 'pk_test_51NXisMDM3y3HeQ88i2SIRHpLikPb7J63tZj5lQvKbK'
-      'sWK3rJQJDs1ZmNX3wvxajQW58bSQmmblaD9YUHh8WZ86jx00qBiTC44O';
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
   await Firebase.initializeApp();
   await init();
+  Stripe.publishableKey = dotenv.env['STRIPE_PUBLIC_KEY'] ?? '';
   await Stripe.instance.applySettings();
   FirebaseChatCore.instance.setConfig(
     const FirebaseChatCoreConfig(null, 'chat_rooms', 'chat_users'),
