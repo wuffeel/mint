ed37283f794402b50feb85d4aad5a0c37da013e8
wuffeel <vbl06402@gmail.com>
2023-08-15 17:52:05 +0300

    Add background and terminated messages listener

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_firebase_chat_core/flutter_firebase_chat_core.dart';
@@ -9,6 +12,22 @@ import 'package:mint/presentation/app/app.dart';
 
 import 'injector/injector.dart';
 
+/// According to Firebase docs, when handling background/terminated notifications
+/// there are two things to keep in mind:
+/// 1. It must not be an anonymous function.
+/// 2. It must be a top-level function (e.g. not a class method which requires
+/// initialization).
+///
+/// Attempts on wrapping [FirebaseMessaging.onBackgroundMessage] in a function
+/// will cause 'Null check operator was used on a null value' exception.
+///
+/// See https://firebase.flutter.dev/docs/messaging/usage/#background-messages
+@pragma('vm:entry-point')
+Future<void> _backgroundMessageHandler(RemoteMessage message) async {
+  // TODO(wuffeel): handle background & terminated notifications
+  log('handleBackgroundMessage: ${message.toMap()}');
+}
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
@@ -18,6 +37,7 @@ Future<void> main() async {
   FirebaseChatCore.instance.setConfig(
     const FirebaseChatCoreConfig(null, 'chat_rooms', 'chat_users'),
   );
+  FirebaseMessaging.onBackgroundMessage(_backgroundMessageHandler);
   configureDependencies();
   await bootstrap(App.new);
 }
