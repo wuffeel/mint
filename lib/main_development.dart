54cd6b169386db49a9d0d5a6660cae657475e65c
wuffeel <vbl06402@gmail.com>
2023-08-18 13:42:44 +0300

    Remove useless listener

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -12,22 +12,6 @@ import 'package:mint/presentation/app/app.dart';
 
 import 'injector/injector.dart';
 
-/// According to Firebase docs, when handling background/terminated notifications
-/// there are two things to keep in mind:
-/// 1. It must not be an anonymous function.
-/// 2. It must be a top-level function (e.g. not a class method which requires
-/// initialization).
-///
-/// Attempts on wrapping [FirebaseMessaging.onBackgroundMessage] in a function
-/// will cause 'Null check operator was used on a null value' exception.
-///
-/// See https://firebase.flutter.dev/docs/messaging/usage/#background-messages
-@pragma('vm:entry-point')
-Future<void> _backgroundMessageHandler(RemoteMessage message) async {
-  // TODO(wuffeel): handle background & terminated notifications
-  log('handleBackgroundMessage: ${message.toMap()}');
-}
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
@@ -37,7 +21,6 @@ Future<void> main() async {
   FirebaseChatCore.instance.setConfig(
     const FirebaseChatCoreConfig(null, 'chat_rooms', 'chat_users'),
   );
-  FirebaseMessaging.onBackgroundMessage(_backgroundMessageHandler);
   configureDependencies();
   await bootstrap(App.new);
 }
