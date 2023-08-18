5e17bf0c3d76b913677879d6521ea0de861e1c2d
wuffeel <vbl06402@gmail.com>
2023-08-18 13:44:38 +0300

    Remove useless imports

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,7 +1,4 @@
-import 'dart:developer';
-
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_firebase_chat_core/flutter_firebase_chat_core.dart';
