c6ffa4405c18e3f50affa516ace2ae17003f105a
wuffeel <vbl06402@gmail.com>
2023-08-25 09:49:04 +0300

    Fix profile app bar

--- lib/presentation/pages/main/profile/widgets/profile_app_bar.dart
+++ lib/presentation/pages/main/profile/widgets/profile_app_bar.dart
@@ -9,6 +9,7 @@ import 'package:mint/presentation/widgets/mint_back_button.dart';
 import 'package:mint/theme/mint_text_styles.dart';
 
 import '../../../../../bloc/user/user_bloc.dart';
+import '../../../../../domain/entity/user_model/user_model.dart';
 
 class ProfileAppBar extends StatelessWidget {
   const ProfileAppBar({
@@ -18,6 +19,7 @@ class ProfileAppBar extends StatelessWidget {
     this.collapseFactor = 0.8,
     this.onPickPhoto,
     this.localPhotoUrl,
+    this.avatarSize = 110,
   })  : assert(
           collapseFactor > 0 && collapseFactor < 1,
           'collapseFactor should be in range of 0.0 to 1.0. '
@@ -56,10 +58,14 @@ class ProfileAppBar extends StatelessWidget {
   /// If null, current user's photo will be displayed.
   final String? localPhotoUrl;
 
+  /// Circle avatar radius
+  final double avatarSize;
+
   @override
   Widget build(BuildContext context) {
     return SliverPersistentHeader(
       delegate: _ProfileHeaderDelegate(
+        avatarSize: avatarSize,
         minHeight: minHeight,
         maxHeight: maxHeight,
         collapseFactor: collapseFactor,
@@ -73,6 +79,7 @@ class ProfileAppBar extends StatelessWidget {
 
 class _ProfileHeaderDelegate extends SliverPersistentHeaderDelegate {
   _ProfileHeaderDelegate({
+    required this.avatarSize,
     required this.minHeight,
     required this.maxHeight,
     required this.collapseFactor,
@@ -85,6 +92,7 @@ class _ProfileHeaderDelegate extends SliverPersistentHeaderDelegate {
   final double collapseFactor;
   final VoidCallback? onPickPhoto;
   final String? localPhotoUrl;
+  final double avatarSize;
 
   @override
   double get minExtent => minHeight;
@@ -92,18 +100,33 @@ class _ProfileHeaderDelegate extends SliverPersistentHeaderDelegate {
   @override
   double get maxExtent => maxHeight;
 
+  /// Calculates the configuration values for the header based on the current
+  /// shrink offset.
+  ///
+  /// The configuration includes the percentage of shrinkage, scale factor for
+  /// animation, text scale factor, and the radius of the avatar.
+  _HeaderConfig _calculateHeaderConfig(double shrinkOffset) {
+    final percentage = (shrinkOffset / (maxExtent - minExtent)).clamp(0.0, 1.0);
+    final scaleFactor = 1 - percentage;
+    final textScaleFactor = 1 - percentage * 0.6;
+    final avatarRadius =
+        percentage >= collapseFactor ? 32.w : 110.w * scaleFactor;
+
+    return _HeaderConfig(
+      percentage: percentage,
+      scaleFactor: scaleFactor,
+      textScaleFactor: textScaleFactor,
+      avatarRadius: avatarRadius,
+    );
+  }
+
   @override
   Widget build(
     BuildContext context,
     double shrinkOffset,
     bool overlapsContent,
   ) {
-    final percentage = (shrinkOffset / (maxExtent - minExtent)).clamp(0.0, 1.0);
-    final scaleFactor = 1 - percentage;
-    final textScaleFactor = 1 - percentage * 0.6;
-    var avatarRadius = 110.w * scaleFactor;
-
-    if (percentage >= collapseFactor) avatarRadius = 32.w;
+    final headerConfig = _calculateHeaderConfig(shrinkOffset);
 
     return BlocBuilder<UserBloc, UserState>(
       builder: (context, state) {
@@ -126,90 +149,29 @@ class _ProfileHeaderDelegate extends SliverPersistentHeaderDelegate {
           );
         }
         if (state is UserFetchSuccess) {
-          final phone = state.user.phoneNumber;
           return ColoredBox(
             color: Theme.of(context).colorScheme.primary,
             child: SafeArea(
               bottom: false,
               child: AnimatedSwitcher(
                 duration: const Duration(milliseconds: 300),
-                child: percentage >= collapseFactor
-                    ? Row(
-                        children: <Widget>[
-                          if (context.router.canPop())
-                            Padding(
-                              padding: EdgeInsets.only(left: 8.w),
-                              child: const MintBackButton(color: Colors.white),
-                            ),
-                          SizedBox(width: 16.w),
-                          ProfileAvatar(
-                            size: avatarRadius,
-                            scaleFactor: scaleFactor,
-                            localPhotoUrl: localPhotoUrl,
-                            photoUrl: state.user.photoUrl,
-                          ),
-                          SizedBox(width: 8.w),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              _UserNameText(
-                                firstName: state.user.firstName,
-                                lastName: state.user.lastName,
-                                fontSize: 16.sp,
-                              ),
-                              if (phone != null)
-                                _PhoneNumberText(
-                                  phoneNumber: phone,
-                                  fontSize: 11.sp,
-                                ),
-                            ],
-                          ),
-                        ],
+                child: headerConfig.percentage >= collapseFactor
+                    ? _CollapsedAppBar(
+                        headerConfig: headerConfig,
+                        localPhotoUrl: localPhotoUrl,
+                        user: state.user,
                       )
-                    : Stack(
-                        alignment: Alignment.center,
-                        children: <Widget>[
-                          if (context.router.canPop(ignoreParentRoutes: true))
-                            Align(
-                              alignment: Alignment.topLeft,
-                              child: Padding(
-                                padding: EdgeInsets.only(left: 8.w, top: 8.h),
-                                child: const MintBackButton(
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              ProfileAvatar(
-                                size: avatarRadius,
-                                scaleFactor: scaleFactor,
-                                onPickPhoto: onPickPhoto,
-                                localPhotoUrl: localPhotoUrl,
-                                photoUrl: state.user.photoUrl,
-                              ),
-                              SizedBox(height: 5.h),
-                              _UserNameText(
-                                firstName: state.user.firstName,
-                                lastName: state.user.lastName,
-                                fontSize: 20.sp * textScaleFactor,
-                              ),
-                              if (phone != null)
-                                _PhoneNumberText(
-                                  phoneNumber: phone,
-                                  fontSize: 13.sp * textScaleFactor,
-                                ),
-                            ],
-                          ),
-                        ],
+                    : _ExpandedAppBar(
+                        headerConfig: headerConfig,
+                        localPhotoUrl: localPhotoUrl,
+                        onPickPhoto: onPickPhoto,
+                        user: state.user,
                       ),
               ),
             ),
           );
         }
-        return const SizedBox.shrink();
+        return _AppBarContainer(maxHeight: maxHeight);
       },
     );
   }
@@ -220,11 +182,118 @@ class _ProfileHeaderDelegate extends SliverPersistentHeaderDelegate {
   }
 }
 
+class _CollapsedAppBar extends StatelessWidget {
+  const _CollapsedAppBar({
+    required this.headerConfig,
+    this.localPhotoUrl,
+    required this.user,
+  });
+
+  final _HeaderConfig headerConfig;
+  final String? localPhotoUrl;
+  final UserModel user;
+
+  @override
+  Widget build(BuildContext context) {
+    final phone = user.phoneNumber;
+    return Row(
+      children: <Widget>[
+        if (context.router.canPop())
+          Padding(
+            padding: EdgeInsets.only(left: 8.w),
+            child: const MintBackButton(color: Colors.white),
+          ),
+        SizedBox(width: 16.w),
+        ProfileAvatar(
+          size: headerConfig.avatarRadius,
+          scaleFactor: headerConfig.scaleFactor,
+          localPhotoUrl: localPhotoUrl,
+          photoUrl: user.photoUrl,
+        ),
+        SizedBox(width: 8.w),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _UserNameText(
+              firstName: user.firstName,
+              lastName: user.lastName,
+              fontSize: 16.sp,
+            ),
+            if (phone != null)
+              _PhoneNumberText(
+                phoneNumber: phone,
+                fontSize: 11.sp,
+              ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _ExpandedAppBar extends StatelessWidget {
+  const _ExpandedAppBar({
+    required this.headerConfig,
+    required this.user,
+    this.localPhotoUrl,
+    this.onPickPhoto,
+  });
+
+  final _HeaderConfig headerConfig;
+  final UserModel user;
+  final VoidCallback? onPickPhoto;
+  final String? localPhotoUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    final phone = user.phoneNumber;
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        if (context.router.canPop(ignoreParentRoutes: true))
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: EdgeInsets.only(left: 8.w, top: 8.h),
+              child: const MintBackButton(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ProfileAvatar(
+              size: headerConfig.avatarRadius,
+              scaleFactor: headerConfig.scaleFactor,
+              onPickPhoto: onPickPhoto,
+              localPhotoUrl: localPhotoUrl,
+              photoUrl: user.photoUrl,
+            ),
+            SizedBox(height: 5.h),
+            _UserNameText(
+              firstName: user.firstName,
+              lastName: user.lastName,
+              fontSize: 20.sp * headerConfig.textScaleFactor,
+            ),
+            if (phone != null)
+              _PhoneNumberText(
+                phoneNumber: phone,
+                fontSize: 13.sp * headerConfig.textScaleFactor,
+              ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
 class _AppBarContainer extends StatelessWidget {
-  const _AppBarContainer({required this.maxHeight, required this.child});
+  const _AppBarContainer({required this.maxHeight, this.child});
 
   final double maxHeight;
-  final Widget child;
+  final Widget? child;
 
   @override
   Widget build(BuildContext context) {
@@ -300,4 +369,27 @@ class _PhoneNumberText extends StatelessWidget {
       ),
     );
   }
+}
+
+/// Configuration values for the header's appearance and animation.
+class _HeaderConfig {
+  _HeaderConfig({
+    required this.percentage,
+    required this.scaleFactor,
+    required this.textScaleFactor,
+    required this.avatarRadius,
+  });
+
+  /// The percentage of shrinkage, ranging from 0.0 (not shrunk) to 1.0
+  /// (fully shrunk).
+  final double percentage;
+
+  /// The scale factor for various animations based on the shrink percentage.
+  final double scaleFactor;
+
+  /// The scale factor for text animations based on the shrink percentage.
+  final double textScaleFactor;
+
+  /// The radius of the avatar image based on the shrink percentage.
+  final double avatarRadius;
 }
