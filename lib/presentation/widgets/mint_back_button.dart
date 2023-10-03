e11040a323fa66e73a4ca9f276592554e95d3590
Vladyslav Blyzniuk <vbl06402@gmail.com>
2023-10-03 11:33:36 +0300

    Reduce size of back button icon to standard material spec

--- lib/presentation/widgets/mint_back_button.dart
+++ lib/presentation/widgets/mint_back_button.dart
@@ -32,8 +32,8 @@ class MintBackButton extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           Container(
-            width: 28.w,
-            height: 28.h,
+            width: 24.w,
+            height: 24.h,
             alignment: Alignment.center,
             child: _getIcon(context).svg(
               colorFilter: ColorFilter.mode(
